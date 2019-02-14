4f0230e251fe368aa639b82dab730b628efde5d1
rich <richard.wota@gmail.com>
2019-02-15 02:02:57 +0900

    added connect server, copy task to grunt. added basic styles and stylesheet imports

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,7 +6,7 @@ module.exports = function(grunt) {
           style: 'expanded'
         },
         files: {                         // Dictionary of files
-          'dist/css/main.css': 'src/css/main.scss',       // 'destination': 'source'
+          'dev/css/main.css': 'src/css/main.scss',       // 'destination': 'source'
         }
       }
     },
@@ -14,7 +14,7 @@ module.exports = function(grunt) {
         all: {
             files: {
                 // Takes the input file `app.css`, and generates `app-old-ie.css`.
-              'dist/css/main-old-ie.css': ['src/css/main.css']
+              'dev/css/main-old-ie.css': ['src/css/main.css']
             }
         }
     },
@@ -23,13 +23,14 @@ module.exports = function(grunt) {
         options: {
           data: {
             debug: false
-          }
+          },
+          pretty: true,
         },
         files: [{
           expand: true,
           cwd: '.',
           src: ['src/pug/*.pug'],
-          dest: 'dist/',
+          dest: 'dev/',
           ext: '.html',
           rename: function(dest, src) {
             src = src.replace('src/pug/', '');
@@ -42,7 +43,7 @@ module.exports = function(grunt) {
     watch: {
       scripts: {
         files: ['src/**/*'],
-        tasks: ['sass', 'pug', 'htmllint'],
+        tasks: ['sass', 'pug', 'htmllint', 'copy'],
         options: {
           spawn: false,
         },
@@ -55,11 +56,36 @@ module.exports = function(grunt) {
         options: {
           server: {}
         },
-        src: 'dist/**.html'
+        src: 'dev/**.html'
       }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 9001,
+          host: 'localhost',
+          base: 'dev',
+          livereload: false, //TODO enable livereload/browser-sync
+        }
+      }
+    },
+    copy: {
+      main: {
+        files: [
+          // includes files within path
+          {
+            expand: true, 
+            flatten: true, 
+            src: ['src/images/**'], 
+            dest: 'dev/images/', 
+            filter: 'isFile'
+          },
+        ],
+      },
     },
   });
 
+  //TODO setup dist task to minimise and uglify assets
   grunt.loadNpmTasks('grunt-contrib-sass');
   //ensure users on IE 8 and below get to view the desktop experience on mobile devices
   grunt.loadNpmTasks('grunt-stripmq');
@@ -67,7 +93,9 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-html');
   grunt.loadNpmTasks('grunt-vnuserver');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
   // Default task(s).
-  grunt.registerTask('default', ['vnuserver', 'watch']);
+  grunt.registerTask('default', ['vnuserver', 'connect', 'watch']);
 };
