e4c8990532576a2acddba82dd894b06fb7a289ef
rich <richard.yuwono@gmail.com>
2019-04-22 00:43:56 +0900

    added template for counselling page to wp

--- wp-content/themes/npoposse/front-page.php
+++ wp-content/themes/npoposse/front-page.php
@@ -52,26 +52,9 @@
         </div>
       </section>
       <section class="links container">
-        <div class="row">
-          <div class="links-container col-md">
-            <div class="centre-text">
-              <div class="circle-icon"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-activity.png" alt="icon activity content"></div><a class="link" href="#">活動内容</a>
-            </div>
-            <div class="content">POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。労働や生活に関する相談を無料で受け付けています。</div>
-          </div>
-          <div class="links-container col-md">
-            <div class="centre-text">
-              <div class="circle-icon"><img class="icon-resolution img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-resolution.png" alt="icon case resolution"></div><a class="link" href="#">解決事例</a>
-            </div>
-            <div class="content">POSSEには年間2000件以上の労働・生活相談が寄せらています。解決事例の一部をご紹介します。</div>
-          </div>
-          <div class="links-container col-md">
-            <div class="centre-text">
-              <div class="circle-icon"><img class="icon-qa img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-qa.png" alt="icon question and answer"></div><a class="link" href="#">相談の流れ・よくある質問</a>
-            </div>
-            <div class="content">相談の流れとよくある質問についてご紹介します。</div>
-          </div>
-        </div>
+
+        <?php get_template_part( 'links', get_post_format() ); ?>
+
       </section>
       <div class="container latest">
         <div class="centre-text"> 
---
+++ wp-content/themes/npoposse/page-labor-counseling.php
@@ -0,0 +1,216 @@
+<?php get_header(); ?>
+
+      <div class="counselling-top">
+        <div class="row counselling">
+          <div class="col-md">
+            <div class="centre-text">
+              <div class="main">労働相談</div>
+            </div>
+          </div>
+          <div class="marquee">
+            <picture>
+              <source srcset="wp-content/themes/npoposse/images/counselling/counselling-marquee-sm.jpg" media="(max-width: 768px)">
+              <source srcset="wp-content/themes/npoposse/images/counselling/counselling-marquee.jpg" media="(min: 769px)"><img src="wp-content/themes/npoposse/images/counselling/counselling-marquee.jpg" alt="marquee image">
+            </picture>
+          </div>
+        </div>
+        <section class="opener">
+          <div class="container">
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="lead">労働トラブルについて<br>NPO法人POSSEでは<br>相談を無料で受け付けています。 </div>
+                  <div class="lead-text">
+                    <p>残業代が支払われない | 有給休暇がとれない | 休みがとれない</p>
+                    <p>セクハラ、パワハラ、嫌がらせをされた | 仕事をやめさせられそうになっている | 仕事をやめさせてもらえない</p>
+                    <p>そのほか、どんなことでも構いません。</p>
+                    <p>ひとりで悩む前に、ぜひためしに一度、お気軽にご連絡ください。</p>
+                  </div>
+                </div><a class="green btn btn-primary" href="#">相談フォームに進む</a>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="solutions">
+          <div class="container">
+            <div class="row head">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="lead">解決のためにPOSSEができること</div>
+                </div>
+                <div class="separator"></div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="icon circle-icon"><img class="img" src="wp-content/themes/npoposse/images/counselling/advice.png" alt="book icon"></div>
+                  <div class="lead">法律の知識、<br>解決法のアドバイス</div>
+                </div>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md">
+                      <div class="content">
+                         ご連絡いただいた内容を元に、相談内容に応じた法律の知識や、解決の仕方をアドバイスいたします。</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="icon circle-icon"><img class="img" src="wp-content/themes/npoposse/images/counselling/support.png" alt="raised hands icon"></div>
+                  <div class="lead">解決のための<br>具体的なサポート</div>
+                </div>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md">
+                      <div class="content">
+                         職場がなかなか適切な対応をとらないケースや、あまりに悪質なケースは、NPO法人POSSEのスタッフが解決のための具体的なサポートをいたします。</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="icon circle-icon"><img class="img" src="wp-content/themes/npoposse/images/counselling/help.png" alt="smart lady icon"></div>
+                  <div class="lead">専門家の紹介</div>
+                </div>
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md">
+                      <div class="content">
+                         問題に応じて、ユニオン（労働組合）や弁護士などの専門家を紹介することもあります。</div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="experts">
+          <div class="container">
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="lead">相談の流れ</div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <picture>
+                    <source srcset="wp-content/themes/npoposse/images/counselling/flow-vertical.png" media="(max-width: 768px)">
+                    <source srcset="wp-content/themes/npoposse/images/counselling/flow-horizontal.png" media="(min-width: 769px)"><img src="wp-content/themes/npoposse/images/counselling/flow-horizontal.png" alt="flowchart">
+                  </picture>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="bottom-link"><a class="link link-caret" href="#" alt="">相談の流れについてもっと詳しく</a></div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="characteristics">
+          <div class="container">
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="title">POSSEの特徴</div>
+                </div>
+                <div class="separator"></div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md panel"><img class="img" src="wp-content/themes/npoposse/images/counselling/characteristics1.png" alt="characteristics">
+                <div class="detail">
+                  <div class="lead"><span>POSSEの特徴</span>を伝えられるようなテキストを入れたいです。</div>
+                  <div class="content">POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。労働や生活に関する相談を無料で受け付けています。POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。</div>
+                </div>
+              </div>
+              <div class="col-md panel"><img class="img" src="wp-content/themes/npoposse/images/counselling/characteristics1.png" alt="characteristics">
+                <div class="detail">
+                  <div class="lead"><span>POSSEの特徴</span>を伝えられるようなテキストを入れたいです。</div>
+                  <div class="content">POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。労働や生活に関する相談を無料で受け付けています。POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。</div>
+                </div>
+              </div>
+              <div class="col-md panel"><img class="img" src="wp-content/themes/npoposse/images/counselling/characteristics1.png" alt="characteristics">
+                <div class="detail">
+                  <div class="lead"><span>POSSEの特徴</span>を伝えられるようなテキストを入れたいです。</div>
+                  <div class="content">POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。労働や生活に関する相談を無料で受け付けています。POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。</div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="contact-cta">
+          <div class="container">
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="title">お気軽にご相談ください</div>
+                  <div class="content">相談無料  /  秘密厳守  /  メール・電話OK</div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md-4">
+                <div class="panel"><a class="btn btn-primary" href="#"><img class="icon" src="wp-content/themes/npoposse/images/counselling/envelope-icn.png" alt="envelope icon">相談フォームに進む</a>
+                  <div class="note-text">
+                     ※勤務先のＰＣからのご連絡はお控えください。相談員からの返信や通信履歴等から、労働相談をしたことや相談内容が勤務先へ漏れる可能性があります。</div>
+                </div>
+              </div>
+              <div class="col-md-8">
+                <div class="panel">
+                  <div class="centre-text">
+                    <div class="hours">平日17:00-22:00 / 土日祝12:00-22:00 火曜定休</div>
+                    <div class="phone-no">03-6699-9359</div>
+                    <div class="phone-sub">
+                      <div class="content">
+                         東北地方にお住まいの方はこちら </div>
+                      <div class="number">022-302-3349</div>
+                    </div>
+                  </div>
+                  <ul>
+                    <li>
+                       現在、相談の増加により、電話がつながりにくくなっております。 　電話がつながらなかった場合は、お手数ですが、留守番電話へメッセージを残していただくか、メール相談フォームにて相談をお送りください。</li>
+                    <li>
+                       留守番電話の場合は、①労働相談をご希望であることと、②端的なご相談内容を残していただきましたら、順次折り返しご連絡をさせていただきます。</li>
+                  </ul>
+                  <div class="note-text">
+                     ※勤務先の固定電話、業務用の携帯電話からのご連絡はお控えください。相談員からの折り返しの電話連絡や通信履歴等から、労働相談をしたことが勤務先へ漏れる可能性があります。</div>
+                </div>
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-md bottom-notes">
+                <div class="note-text">※来所相談をご希望の方は、相談スタッフが相談等で事務所に不在の場合もございますので、必ず事前にメールまたは電話にてご連絡をお願いいたします。</div>
+                <div class="note-text">※ご相談内容は、個人が特定されない形で調査等に利用する場合がございますので、ご了承ください。</div>
+              </div>
+            </div>
+          </div>
+        </section>
+        <section class="about">
+          <div class="container links">
+            <div class="row">
+              <div class="col-md">
+                <div class="centre-text">
+                  <div class="title">NPO法人<br>POSSEについて</div>
+                </div>
+                <div class="separator"></div>
+              </div>
+            </div>
+
+            <?php get_template_part( 'links', get_post_format() ); ?>
+
+
+          </div>
+        </section>
+      </div>
+
+
+<?php get_footer(); ?>
