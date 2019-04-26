3c5bda711010d8841cb4b7fa548dad4a78f07938
rich <richard.yuwono@gmail.com>
2019-04-26 21:01:40 +0900

    updated activities page in theme

---
+++ wp-content/themes/npoposse/contact-cta.php
@@ -0,0 +1,48 @@
+<section class="contact-cta">
+  <div class="container">
+    <div class="row">
+      <div class="col-md">
+        <div class="centre-text">
+          <div class="title">お気軽にご相談ください</div>
+          <div class="content">相談無料  /  秘密厳守  /  メール・電話OK</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md-4">
+        <div class="panel"><a class="btn btn-primary" href="#"><img class="icon" src="<?php bloginfo('stylesheet_directory'); ?>/images/counselling/envelope-icn.png" alt="envelope icon">相談フォームに進む</a>
+          <div class="note-text">
+             ※勤務先のＰＣからのご連絡はお控えください。相談員からの返信や通信履歴等から、労働相談をしたことや相談内容が勤務先へ漏れる可能性があります。</div>
+        </div>
+      </div>
+      <div class="col-md-8">
+        <div class="panel">
+          <div class="centre-text">
+            <div class="hours">平日17:00-22:00 / 土日祝12:00-22:00 火曜定休</div>
+            <div class="phone-no">03-6699-9359</div>
+            <div class="phone-sub">
+              <div class="content">
+                 東北地方にお住まいの方はこちら </div>
+              <div class="number">022-302-3349</div>
+            </div>
+          </div>
+          <ul>
+            <li>
+               現在、相談の増加により、電話がつながりにくくなっております。 　電話がつながらなかった場合は、お手数ですが、留守番電話へメッセージを残していただくか、メール相談フォームにて相談をお送りください。</li>
+            <li>
+               留守番電話の場合は、①労働相談をご希望であることと、②端的なご相談内容を残していただきましたら、順次折り返しご連絡をさせていただきます。</li>
+          </ul>
+          <div class="note-text">
+             ※勤務先の固定電話、業務用の携帯電話からのご連絡はお控えください。相談員からの折り返しの電話連絡や通信履歴等から、労働相談をしたことが勤務先へ漏れる可能性があります。</div>
+        </div>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-md bottom-notes">
+        <div class="note-text">※来所相談をご希望の方は、相談スタッフが相談等で事務所に不在の場合もございますので、必ず事前にメールまたは電話にてご連絡をお願いいたします。</div>
+        <div class="note-text">※ご相談内容は、個人が特定されない形で調査等に利用する場合がございますので、ご了承ください。</div>
+      </div>
+    </div>
+  </div>
+</section>
+
---
+++ wp-content/themes/npoposse/page-activities.php
@@ -0,0 +1,230 @@
+<?php get_header(); ?>
+
+      <div class="activities-top">
+        <section class="activities-marquee">
+          <div class="marquee"><img src="<?php bloginfo('stylesheet_directory'); ?>/images/home/about.jpg" alt="activities image">
+            <div class="overlay"></div>
+            <div class="marquee-text">活動内容</div>
+          </div>
+        </section>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-9">
+            <div class="activities-top">
+              <div class="container intro">
+                <div class="row">
+                  <div class="col-md content">NPO法人POSSE（ポッセ）は、誰もが安心して生活し、働くことができる社会を目指して、労働や貧困問題に取り組む団体です。</div>
+                </div>
+                <div class="row">
+                  <div class="col-md">
+                    <div class="separator-container">
+                      <div class="separator"></div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="container about-org">
+                <div class="row">
+                  <div class="col-md">
+                    <h1 class="title">POSSEの活動について</h1>
+                    <div class="content">POSSEでは、相談へのアドバイス、権利行使のサポートに加え、メディアでの発信などをつうじて、労働・貧困の現場から社会に影響を与えていく活動を行っています。</div>
+                  </div>
+                </div>
+              </div>
+              <div class="container activity-content">
+                <div class="row">
+                  <div class="col-md">
+                    <h2>活動内容</h2>
+                  </div>
+                </div>
+                <div class="row content-row">
+                  <div class="col-md-4"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/consultation.jpg" alt="consultation"></div>
+                  <div class="col-md-8">
+                    <div class="lead">労働・生活相談</div>
+                    <div class="content">POSSEでは、労働問題、生活支援、奨学金の相談を無料で行っています。件数は年間2000件以上を数えます。 電話やメール、面談を通じての法律知識や解決方法のアドバイス・サポートを中心に、労働組合や弁護士、労働基準監督署などの紹介なども行っています。</div>
+                  </div>
+                </div>
+                <div class="row content-row">
+                  <div class="col-md-4"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/labour-law.jpg" alt="labour law education"></div>
+                  <div class="col-md-8">
+                    <div class="lead">労働法教育</div>
+                    <div class="content">高校や大学などの学生を主な対象として、労働法について累計100回以上の講義をしています。 労働法の条文の紹介ではなく、ワークショップやロールプレイの実践、実際の労働問題の映像の上映、学生が想定しやすいブラックバイトでの権利行使の練習などの工夫をしています。</div>
+                  </div>
+                </div>
+                <div class="row content-row">
+                  <div class="col-md"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/magazine.jpg" alt="magazine production"></div>
+                  <div class="col-md-8">
+                    <div class="lead">雑誌制作</div>
+                    <div class="content">年4回発行の季刊誌として、雇用問題総合誌『POSSE』を発行してきました。労働・貧困問題の実態を伝えるルポ・調査や、政策提言や背景分析を論じる研究者の論文・インタビュー・座談会など、多彩なテキストを掲載しています。2008年の創刊以来、朝日新聞の論壇時評で30回以上紹介されるなど、NPO発行の雑誌でありながら、雇用・貧困論壇で注目され続けています。</div>
+                  </div>
+                </div>
+              </div>
+              <section class="awards">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md">
+                      <h2>受賞歴</h2>
+                      <table>
+                        <tr>
+                          <th>2013年</th>
+                          <td>ユーキャン新語・流行語大賞トップテン：POSSE代表・今野晴貴が「ブラック企業」で受賞</td>
+                        </tr>
+                        <tr>
+                          <th>2013年</th>
+                          <td>大佛次郎論壇賞：POSSE代表・今野晴貴が『ブラック企業　日本を食いつぶす妖怪』（2012年、文春新書）で受賞</td>
+                        </tr>
+                        <tr>
+                          <th>2013年</th>
+                          <td>仙台市において実施したPOSSEの被災者送迎事業に対して、厚生労働大臣から感謝状を受領</td>
+                        </tr>
+                      </table>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section class="history">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md">
+                      <h2>助成金</h2>
+                      <div class="content">
+                        <p>
+                           POSSE発足以来、各種団体から助成金を受領しています。 </p>
+                        <p>代表例</p>
+                        <p>電通育英会（生活相談）　/　パルシステム東京市民活動助成基金（生活相談）　/　市民社会チャレンジ基金（政策提言）　/　草の根市民基金・ぐらん（政策提言）　/　みやぎNPO夢ファンド（広報活動）　/　大阪コミュニティ財団（労働法教育）　/　福祉医療機構（被災地支援）　/　赤い羽根共同募金（被災地支援事業）　/　JKA（被災地支援事業）　ほか多数</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md">
+                      <h2>沿革</h2>
+                      <div class="content">
+                        <p>2000年代後半、若者の格差・貧困問題が注目され、特に「非正規雇用」の問題がクローズアップされました。</p>
+                        <p>POSSEは、その最中である2006年に、当時大学生だった今野晴貴らによって結成され、労働相談や街頭調査などを通じて、これらの問題に取り組んできました。</p>
+                        <p>それから10年が経ち、雇用の劣化はますます拡大しています。</p>
+                        <p>非正規雇用に限らず、正社員であっても、低賃金・長時間労働が「普通」のことになり、誰もが貧困や過労死と隣り合わせの時代になっています。</p>
+                        <p>私たちは、このことを早くから「ブラック企業」問題として位置付け、活動をつうじて社会問題化してきました。</p>
+                        <p>これまで1万件を超える相談の解決支援や、労働・貧困の実態の啓発、求められる政策の提言を続けています。</p>
+                        <p>2014年にはPOSSEのスタッフが中心となり、個人加盟の労働組合・総合サポートユニオンが設立されています。</p>
+                        <p>同ユニオンでは、2014年の「たかの友梨ビューティクリニック」事件をはじめ、POSSEと連携して事件解決に取り組んでいます。</p>
+                        <p>現在では、エステ・ユニオン、介護・保育ユニオン、ブラックバイトユニオン、ブラック企業ユニオンなどの業種別・課題別の支部がつくられています。</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md">
+                      <div class="separator-container">
+                        <div class="separator"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section class="about-consultation">
+                <div class="container">
+                  <div class="row">
+                    <div class="col-md">
+                      <h1 class="title">労働・生活相談について</h1>
+                      <div class="content">
+                        <p>POSSEでは労働・生活に関する相談を無料で受け付けております。</p>
+                        <p>POSSEでは、みなさんからの相談を無料で受け付けています。「労働」「生活」「奨学金」についての相談が主となっています。</p>
+                        <p>ご相談の連絡をいただくと、担当のスタッフが相談の問題点を整理して、解決の方向性や、場合によっては専門家・団体をアドバイスします。弁護士や労働組合などを活用される際は、相談者からそれらの専門家・団体に対して、着手金・成功報酬・入会費・組合費などの支払いが一般的に発生しますが、POSSEに対する費用の支払いはありません。POSSEはNPOとしての会費・寄付・各種助成金による収入や、ボランティアに支えられて活動を運営しているため、無料での相談対応となっています。</p>
+                        <p>POSSEに寄せられた相談から、労働基準監督署の行政指導、裁判所での民事訴訟、労働組合での団体交渉をつうじて解決に結びついた事例は、テレビ・新聞・雑誌などでも多数取り上げられています。事件解決までの概要は、「<a href="#">解決事例</a>」をご参照ください。</p>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row">
+                    <div class="col-md">
+                      <div class="separator-container">
+                        <div class="separator"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+              <section class="staff">
+                <div class="container">
+                  <div class="row title-row">
+                    <div class="col-md">
+                      <h1 class="title">スタッフ</h1>
+                      <div class="content">POSSEの主要スタッフをご紹介いたします。</div>
+                    </div>
+                  </div>
+                  <div class="row profile-row">
+                    <div class="col-md-4">
+                      <div class="profile"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/konno.jpg" alt="konno-san"></div>
+                    </div>
+                    <div class="col-md-8">
+                      <div class="profile">
+                        <div class="role-title">NPO法人POSSE代表</div>
+                        <div class="name">今野晴貴</div>
+                        <div class="bio content">
+                          <p>著書に『ブラック企業』（文春新書）、『ブラックバイト』（岩波新書）など多数。2013年度大佛次郎</p>
+                          <p>論壇賞、流行語大賞トップ10を受賞。</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row profile-row">
+                    <div class="col-md-4">
+                      <div class="profile"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/sakakura.jpg" alt="sakakura-san"></div>
+                    </div>
+                    <div class="col-md-8">
+                      <div class="profile">
+                        <div class="role-title">NPO法人POSSE理事 / 雑誌『POSSE』前編集長 / ブラック企業ユニオン・裁量労働制ユニオン代表</div>
+                        <div class="name">坂倉昇平</div>
+                        <div class="bio content">
+                          <p>共著に『ベーシックインカムは究極の社会保障か』（堀之内出版）、『18歳からの民主主義』（岩波新書）など。</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row profile-row">
+                    <div class="col-md-4">
+                      <div class="profile"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/watanabe.jpg" alt="watanabe-san"></div>
+                    </div>
+                    <div class="col-md-8">
+                      <div class="profile">
+                        <div class="role-title">NPO法人POSSE事務局長/雑誌『POSSE』編集長</div>
+                        <div class="name">渡辺寛人</div>
+                        <div class="bio content">
+                          <p>社会福祉士。共著に『断絶の都市センダイ』（朝日新聞出版）</p>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div class="row profile-row">
+                    <div class="col-md-4">
+                      <div class="profile"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/activities/miura.jpg" alt="miura-san"></div>
+                    </div>
+                    <div class="col-md-8">
+                      <div class="profile">
+                        <div class="role-title">NPO法人POSSE相談スタッフ</div>
+                        <div class="name">三浦かおり </div>
+                        <div class="bio content"></div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </section>
+            </div>
+          </div>
+          <div class="col-lg-3 sidemenu">
+            <ul>
+              <li><a href="#" alt="link">
+                  <div class="menu-item">活動内容</div></a></li>
+              <li><a href="#" alt="link">
+                  <div class="menu-item">解決事例</div></a></li>
+              <li><a href="#" alt="link">
+                  <div class="menu-item">相談の流れとよくある質問</div></a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+
+      <?php get_template_part( 'contact-cta', get_post_format() ); ?>
+
+
+<?php get_footer(); ?>
