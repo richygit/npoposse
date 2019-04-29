d80c10655de7e720c14061a8d9ded8e77b1a130b
rich <richard.yuwono@gmail.com>
2019-04-30 01:58:52 +0900

    add styles for page. page template searches for post which matches the page name. added markup for page template

--- wp-content/themes/npoposse/page.php
+++ wp-content/themes/npoposse/page.php
@@ -1,8 +1,29 @@
 <?php get_header(); ?>
 
-  <?php echo get_the_title(); ?>
+  <?php
 
-  <?php echo get_the_content(); ?>
+    /* Start the Loop */
+    while ( have_posts() ) :
+      the_post();
+      $page_title = get_the_title();
+      $image_page = get_page_by_title(str_replace(' ', '-', $page_title) . '-image', OBJECT, 'post');
+      $marquee_image = apply_filters( 'the_content', $image_page->post_content );
+  ?> 
+    <div class="marquee">
+      <?php  echo $marquee_image ?><!-- marquee image -->
+      <div class="overlay"></div>
+      <h1 class="marquee-text"><?php echo $page_title ?></h1>
+    </div>
 
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <?php the_content(); ?>
+        </div>
+      </div>
+    </div>
+
+  <?php endwhile; // End of the loop.
+  ?>
 
 <?php get_footer(); ?>
