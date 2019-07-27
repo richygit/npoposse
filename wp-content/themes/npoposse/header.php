e28f2b9758388cd7a9d48b24aa6e561bafcab21b
rich <richard.yuwono@gmail.com>
2019-07-28 00:58:56 +0900

    updated links in nav. updated styles for dropdowns in navs

--- wp-content/themes/npoposse/header.php
+++ wp-content/themes/npoposse/header.php
@@ -27,14 +27,47 @@
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#main-menu" aria-controls="main-menu" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
         <div class="collapse navbar-collapse menu-links" id="main-menu">
           <ul class="list navbar-nav">
-            <li class="nav-item active"><a class="nav-link" href="/">ニュース<span class="sr-only">(current)</span></a></li>
-            <li class="nav-item"><a class="nav-link" href="#">POSSEについて</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">相談窓口</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">お問い合わせ</a></li>
-            <li class="nav-item"><a class="nav-link" href="#">POSSEを支援する</a></li>
+            <li class="nav-item active"><a class="nav-link" href="/ニュース">ニュース<span class="sr-only">(current)</span></a></li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="about-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                について
+              </a>
+              <div class="dropdown-menu" aria-labelledby="about-dropdown">
+                <a class="dropdown-item" href="/POSSEについて">POSSEについて</a>
+                <a class="dropdown-item" href="/活動内容">活動内容</a>
+                <a class="dropdown-item" href="/解決事例">解決事例</a>
+                <a class="dropdown-item" href="/相談のながれ">相談のながれ</a>
+              </div>
+            </li>
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="consultation-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                相談窓口
+              </a>
+              <div class="dropdown-menu" aria-labelledby="consultation-dropdown">
+                <a class="dropdown-item" href="/相談窓口">相談窓口</a>
+                <a class="dropdown-item" href="/労働相談">労働相談</a>
+                <a class="dropdown-item" href="/生活相談">生活相談</a>
+              </div>
+            </li>
+
+            <li class="nav-item"><a class="nav-link" href="/お問い合わせ">お問い合わせ</a></li>
+
+
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" id="support-dropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                POSSEを支援する
+              </a>
+              <div class="dropdown-menu" aria-labelledby="support-dropdown">
+                <a class="dropdown-item" href="/POSSEを支援する">POSSEを支援する</a>
+                <a class="dropdown-item" href="/寄付">寄付</a>
+                <a class="dropdown-item" href="/会員制度">会員制度</a>
+              </div>
+            </li>
           </ul>
         </div>
-        <div class="btn-group" role="toolbar" aria-label="生活相談, 労働相談"><a class="counselling btn align-middle" href="#">労働相談</a><a class="consultation btn align-middle" href="#">生活相談</a></div>
+        <div class="btn-group" role="toolbar" aria-label="生活相談, 労働相談"><a class="counselling btn align-middle" href="/労働相談">労働相談</a><a class="consultation btn align-middle" href="/生活相談">生活相談</a></div>
       </nav>
     </header>
     <div class="container-fluid">
