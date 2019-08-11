77db6ea8707a61b7c32cee6e6ab26646ded5a45a
rich <richard.yuwono@gmail.com>
2019-08-11 16:14:13 +0900

    fix links in links template to point to romanji pages

--- wp-content/themes/npoposse/links.php
+++ wp-content/themes/npoposse/links.php
@@ -1,19 +1,19 @@
         <div class="row">
           <div class="links-container col-md">
             <div class="centre-text">
-              <div class="icon circle-icon"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-activity.png" alt="icon activity content"></div><a class="link link-caret" href="/活動内容">活動内容</a>
+              <div class="icon circle-icon"><img class="img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-activity.png" alt="icon activity content"></div><a class="link link-caret" href="/katsudounaiyou">活動内容</a>
             </div>
             <div class="content">POSSE（ポッセ）は若者の労働・貧困問題に取り組むNPO法人です。労働や生活に関する相談を無料で受け付けています。</div>
           </div>
           <div class="links-container col-md">
             <div class="centre-text">
-              <div class="icon circle-icon"><img class="icon-resolution img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-resolution.png" alt="icon case resolution"></div><a class="link link-caret" href="/解決事例">解決事例</a>
+              <div class="icon circle-icon"><img class="icon-resolution img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-resolution.png" alt="icon case resolution"></div><a class="link link-caret" href="/kaiketsujirei">解決事例</a>
             </div>
             <div class="content">POSSEには年間2000件以上の労働・生活相談が寄せらています。解決事例の一部をご紹介します。</div>
           </div>
           <div class="links-container col-md">
             <div class="centre-text">
-              <div class="icon circle-icon"><img class="icon-qa img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-qa.png" alt="icon question and answer"></div><a class="link link-caret" href="/相談のながれ">相談の流れ・よくある質問</a>
+              <div class="icon circle-icon"><img class="icon-qa img" src="<?php bloginfo('stylesheet_directory'); ?>/images/icon-qa.png" alt="icon question and answer"></div><a class="link link-caret" href="/soudannonagare">相談の流れ・よくある質問</a>
             </div>
             <div class="content">相談の流れとよくある質問についてご紹介します。</div>
           </div>
