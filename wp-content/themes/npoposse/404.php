1cb031d976fffed1d67073fe10c1ba3aef80eaac
rich <richard.yuwono@gmail.com>
2019-08-11 19:27:57 +0900

    added 404 page

---
+++ wp-content/themes/npoposse/404.php
@@ -0,0 +1,17 @@
+<?php get_header(); ?>
+
+<div class="container page">
+  <div class="row">
+    <h1>ページが表示できません</h1>
+  </div>
+  <div class="row">
+    <p>
+      お探しのページは一時的にアクセスができない状況にあるか、移動もしくは削除された可能性があります。
+    </p>
+    <p>
+      また、URL、ファイル名にタイプミスがないか再度ご確認ください。 
+    </p>
+  </div>
+</div>
+
+<?php get_footer(); ?>
