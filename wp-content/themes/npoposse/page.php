d009f93523ce280ba3e64230057cf165cc5d9c88
rich <richard.yuwono@gmail.com>
2019-08-11 15:46:06 +0900

    use the meta tags manager plugin to inject meta tags into the page. use the meta tag to point to the image to use as the header image.

--- wp-content/themes/npoposse/page.php
+++ wp-content/themes/npoposse/page.php
@@ -7,10 +7,21 @@
       the_post();
       $page_title = get_the_title();
       $image_page = get_page_by_title(str_replace(' ', '-', $page_title) . '-image', OBJECT, 'post');
-      $marquee_image = apply_filters( 'the_content', $image_page->post_content );
+      $post_id = get_the_ID();
+      $post_meta = get_post_meta($post_id, 'mtm_data')[0];
+
+      $marquee_image = '/wp-content/themes/npoposse/images/default-marquee.jpg';
+
+      if ($post_meta) {
+        foreach($post_meta as $index => $property_array) {
+          if($property_array['value'] == 'marquee') {
+            $marquee_image = $property_array['content'];
+          }
+        }
+      }
   ?> 
     <div class="marquee">
-      <?php  echo $marquee_image ?><!-- marquee image -->
+      <img src="<?php  echo $marquee_image ?>" alt="marquee image" />
       <div class="overlay"></div>
       <h1 class="marquee-text"><?php echo $page_title ?></h1>
     </div>
